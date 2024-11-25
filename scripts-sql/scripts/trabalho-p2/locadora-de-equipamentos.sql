4ec12c633088266c8fe953b06024ccaf37ac0a48
jpcadinelli <jpcadinelli@gmail.com>
2024-11-25 18:49:06 -0300

    wip: subindo trabalho p2

--- scripts-sql/scripts/trabalho-p2/locadora-de-equipamentos.sql
+++ scripts-sql/scripts/trabalho-p2/locadora-de-equipamentos.sql
@@ -58,3 +58,79 @@ CREATE TABLE multa (
         CONSTRAINT pk_multa PRIMARY KEY (id),
         CONSTRAINT fk_multa_locacao FOREIGN KEY (id_locacao) references locacao(id)
 );
+
+CREATE TABLE funcionario (
+    id BIGINT,
+    nome VARCHAR(100),
+    data_contratacao DATE,
+    cargo VARCHAR(100),
+        CONSTRAINT pk_funcionario PRIMARY KEY (id)
+);
+
+CREATE TABLE locacao_funcionario (
+    id BIGINT,
+    id_locacao BIGINT,
+    id_funcionario BIGINT,
+        CONSTRAINT pk_locacao_funcionario PRIMARY KEY (id),
+        CONSTRAINT fk_locacao_funcionario_locacao FOREIGN KEY (id_locacao) references locacao(id),
+        CONSTRAINT fk_locacao_funcionario_funcionario FOREIGN KEY (id_funcionario) references funcionario(id)
+);
+
+CREATE TABLE servico_adicional (
+    id BIGINT,
+    id_locacao BIGINT,
+    nome VARCHAR(100),
+    descricao BLOB,
+    custo DECIMAL(10,2),
+        CONSTRAINT pk_servico_adicional PRIMARY KEY (id),
+        CONSTRAINT fk_servico_adicional_locacao FOREIGN KEY (id_locacao) references locacao(id)
+);
+
+CREATE TABLE categoria (
+    id BIGINT,
+    nome VARCHAR(100),
+    descricao BLOB,
+        CONSTRAINT pk_categoria PRIMARY KEY (id)
+);
+
+CREATE TABLE equipamento (
+    id BIGINT,
+    id_categoria BIGINT,
+    nome VARCHAR(100),
+    marca VARCHAR(100),
+    modelo VARCHAR(100),
+    data_aquisicao DATE,
+    status ENUM('Disponível', 'Locado', 'Em Manutenção'),
+        CONSTRAINT pk_equipamento PRIMARY KEY (id),
+        CONSTRAINT fk_equipamento_categoria FOREIGN KEY (id_categoria) REFERENCES categoria(id)
+);
+
+CREATE TABLE locacao_equipamento (
+    id BIGINT,
+    id_locacao BIGINT,
+    id_equipamento BIGINT,
+        CONSTRAINT pk_locacao_equipamento PRIMARY KEY (id),
+        CONSTRAINT fk_locacao_equipamento_locacao FOREIGN KEY (id_locacao) references locacao(id),
+        CONSTRAINT fk_locacao_equipamento_equipamento FOREIGN KEY (id_equipamento) references equipamento(id)
+);
+
+CREATE TABLE empresa (
+    id BIGINT,
+    nome VARCHAR(100),
+    cnpj VARCHAR(14),
+    orcamento DECIMAL(10,2),
+        CONSTRAINT pk_empresa PRIMARY KEY (id)
+);
+
+CREATE TABLE manutencao (
+    id BIGINT,
+    id_equipamento BIGINT,
+    id_empresa BIGINT,
+    descricao BLOB,
+    data_inicio DATE,
+    data_fim DATE,
+    custo DECIMAL(10,2),
+        CONSTRAINT pk_manutencao PRIMARY KEY (id),
+        CONSTRAINT fk_manutencao_equipamento FOREIGN KEY (id_equipamento) references equipamento(id),
+        CONSTRAINT fk_manutencao_empresa FOREIGN KEY (id_empresa) references empresa(id)
+);
